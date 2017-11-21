ae8cf70f4ae081dd1fa8ca82daf2b6be038ef108
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2017-11-21 11:42:44 +0100

    [WIP] Add 3d tetris

---
+++ src/keyHandler.js
@@ -0,0 +1,29 @@
+'use strict';
+var keys = [];
+var keysToDisable = [];
+document.addEventListener(
+	'keydown',
+	function(event) {
+		keys[event.keyCode] = true;
+		delete keysToDisable[event.keyCode];
+	},
+	false
+);
+document.addEventListener(
+	'keyup',
+	function(event) {
+		keysToDisable[event.keyCode] = true;
+	},
+	false
+);
+
+const keyHandler = {
+	isPressed(key) {
+		return keys[key];
+	},
+	tick() {
+		for (var code in keysToDisable) {
+			delete keys[code];
+		}
+	}
+};
